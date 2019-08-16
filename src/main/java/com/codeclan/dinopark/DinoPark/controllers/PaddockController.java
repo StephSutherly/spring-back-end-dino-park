6d0cfdad2398f636eacb3419d1ee726ef786e829
Robert Tam <rsh.tam@gmail.com>
2019-08-16 15:16:52 +0100

    Add method to look for paddocks with dinos of a certain name

--- src/main/java/com/codeclan/dinopark/DinoPark/controllers/PaddockController.java
+++ src/main/java/com/codeclan/dinopark/DinoPark/controllers/PaddockController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/paddocks")
+@RequestMapping(value="/paddocks")
 public class PaddockController {
 
   @Autowired
