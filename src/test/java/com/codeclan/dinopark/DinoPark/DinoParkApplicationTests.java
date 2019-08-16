1dd1c9642e87fc080ec0a7e6b70dc661d948bc28
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-16 13:51:02 +0100

    Added herbivore enum

--- src/test/java/com/codeclan/dinopark/DinoPark/DinoParkApplicationTests.java
+++ src/test/java/com/codeclan/dinopark/DinoPark/DinoParkApplicationTests.java
@@ -1,5 +1,6 @@
 package com.codeclan.dinopark.DinoPark;
 
+import com.codeclan.dinopark.DinoPark.models.Dinosaur;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
