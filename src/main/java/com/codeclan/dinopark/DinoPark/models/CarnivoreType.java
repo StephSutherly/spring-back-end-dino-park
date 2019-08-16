55a414be333646d5742967dde5bcd6f4c4958807
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-16 13:57:07 +0100

    Added carnivore enum

---
+++ src/main/java/com/codeclan/dinopark/DinoPark/models/CarnivoreType.java
@@ -0,0 +1,12 @@
+package com.codeclan.dinopark.DinoPark.models;
+
+public enum CarnivoreType {
+
+    TYRANNOSAURUS_REX,
+    VELOCIRAPTOR,
+    SPINOSAURUS,
+    PTERODACTYL,
+    GIGANOTOSAURUS,
+    MOSOSAURUS
+    
+}
