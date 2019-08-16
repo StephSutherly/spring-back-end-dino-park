92852ddd288d188ecbf8638043f0c71816ec076b
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-16 15:50:27 +0100

    Added dinosaur controller

---
+++ src/main/java/com/codeclan/dinopark/DinoPark/controllers/DinosaurController.java
@@ -0,0 +1,24 @@
+package com.codeclan.dinopark.DinoPark.controllers;
+
+import com.codeclan.dinopark.DinoPark.models.Dinosaur;
+import com.codeclan.dinopark.DinoPark.repositories.DinosaurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/dinosaurs")
+public class DinosaurController {
+
+    @Autowired
+    DinosaurRepository dinosaurRepository;
+
+    @GetMapping
+    public List<Dinosaur> getAllDinosaurs() {
+        return dinosaurRepository.findAll();
+    }
+
+}
