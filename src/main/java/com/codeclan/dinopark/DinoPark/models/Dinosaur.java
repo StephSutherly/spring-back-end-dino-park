632d5cde5aab72374bb2b4c7b80354b865e7e3cc
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-16 13:41:46 +0100

    Dinosaur class completed

---
+++ src/main/java/com/codeclan/dinopark/DinoPark/models/Dinosaur.java
@@ -0,0 +1,68 @@
+package com.codeclan.dinopark.DinoPark.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="dinosaurs")
+
+public abstract class Dinosaur {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="is_herbivore")
+    private boolean isHerbivore;
+
+    @Column(name="fullness_level")
+    private int fullnessLevel;
+
+    //    private Long paddockId;
+
+
+    public Dinosaur(Long id, String name, boolean isHerbivore, int fullnessLevel) {
+        this.id = id;
+        this.name = name;
+        this.isHerbivore = isHerbivore;
+        this.fullnessLevel = fullnessLevel;
+    }
+
+    public Dinosaur() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHerbivore() {
+        return isHerbivore;
+    }
+
+    public void setHerbivore(boolean herbivore) {
+        isHerbivore = herbivore;
+    }
+
+    public int getFullnessLevel() {
+        return fullnessLevel;
+    }
+
+    public void setFullnessLevel(int fullnessLevel) {
+        this.fullnessLevel = fullnessLevel;
+    }
+}
