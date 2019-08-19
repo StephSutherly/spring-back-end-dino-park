f704528a8815a0ef5d60aa1e1cd17aae6167bbf1
Robert Tam <rsh.tam@gmail.com>
2019-08-19 10:22:36 +0100

    add feed all route to back end

--- src/main/java/com/codeclan/dinopark/DinoPark/controllers/DinosaurController.java
+++ src/main/java/com/codeclan/dinopark/DinoPark/controllers/DinosaurController.java
@@ -3,10 +3,7 @@ package com.codeclan.dinopark.DinoPark.controllers;
 import com.codeclan.dinopark.DinoPark.models.Dinosaur;
 import com.codeclan.dinopark.DinoPark.repositories.dinosaur_repository.DinosaurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +28,15 @@ public class DinosaurController {
     @GetMapping(value = "/paddock/{id}")
     public List<Dinosaur> findDinosaurByPaddock(@PathVariable Long id) {
         return dinosaurRepository.findDinosaurByPaddockId(id);
+    }
+
+    @GetMapping(value = "/feed")
+    public void findAllDinosaursAndUpdateFullnessLevel() {
+        List<Dinosaur> dinosaurs = getAllDinosaurs();
+        for (Dinosaur dinosaur : dinosaurs) {
+            dinosaur.setFullnessLevel(100);
+            dinosaurRepository.save(dinosaur);
+        }
     }
 
 }
