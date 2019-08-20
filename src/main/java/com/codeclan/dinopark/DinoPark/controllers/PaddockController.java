7d9e031aee045c07faf787e3b4bb80d1df203fec
Robert Tam <rsh.tam@gmail.com>
2019-08-20 10:54:41 +0100

    Add mapping for post to paddocks

--- src/main/java/com/codeclan/dinopark/DinoPark/controllers/PaddockController.java
+++ src/main/java/com/codeclan/dinopark/DinoPark/controllers/PaddockController.java
@@ -3,10 +3,7 @@ package com.codeclan.dinopark.DinoPark.controllers;
 import com.codeclan.dinopark.DinoPark.models.Paddock;
 import com.codeclan.dinopark.DinoPark.repositories.paddock_repository.PaddockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +18,11 @@ public class PaddockController {
   @GetMapping
   public List<Paddock> findAllPaddocks() {
     return paddockRepository.findAll();
+  }
+
+  @PostMapping
+  public void addNewPaddock(@RequestBody Paddock paddock) {
+    paddockRepository.save(paddock);
   }
 
   @GetMapping(value="/{id}")
