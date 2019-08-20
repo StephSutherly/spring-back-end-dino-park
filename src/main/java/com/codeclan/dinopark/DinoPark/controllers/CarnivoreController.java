9703cd2b0a748db0c6d377059a06f379aa4dbdea
Robert Tam <rsh.tam@gmail.com>
2019-08-20 15:49:05 +0100

    Add CarnivoreController class and test on browser

---
+++ src/main/java/com/codeclan/dinopark/DinoPark/controllers/CarnivoreController.java
@@ -0,0 +1,32 @@
+package com.codeclan.dinopark.DinoPark.controllers;
+
+import com.codeclan.dinopark.DinoPark.models.Carnivore;
+import com.codeclan.dinopark.DinoPark.repositories.dinosaur_repository.CarnivoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/carnivores")
+public class CarnivoreController {
+
+  @Autowired
+  CarnivoreRepository carnivoreRepository;
+
+  @GetMapping
+  public List<Carnivore> getAllCarnivores() {
+    return carnivoreRepository.findAll();
+  }
+
+  @PostMapping
+  public void addNewCarnivore(Carnivore carnivore) {
+    carnivoreRepository.save(carnivore);
+  }
+
+  @GetMapping(value="/{id}")
+  public Optional<Carnivore> findDinosaur(@PathVariable Long id) {
+    return carnivoreRepository.findById(id);
+  }
+}
