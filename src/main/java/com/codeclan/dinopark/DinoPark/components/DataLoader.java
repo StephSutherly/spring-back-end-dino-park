a375e47acca9255cbe726dd25fc0d2fab6d2faa4
Robert Tam <rsh.tam@gmail.com>
2019-08-21 12:36:29 +0100

    add more paddocks and dinos to database

--- src/main/java/com/codeclan/dinopark/DinoPark/components/DataLoader.java
+++ src/main/java/com/codeclan/dinopark/DinoPark/components/DataLoader.java
@@ -29,8 +29,17 @@ public class DataLoader implements ApplicationRunner {
         Paddock paddockBlue = new Paddock("Grant", true);
         paddockRepository.save(paddockBlue);
 
+        Paddock paddockOrange = new Paddock("Malcolm", true);
+        paddockRepository.save(paddockOrange);
+
         Paddock paddockRed = new Paddock("Muldoon", false);
         paddockRepository.save(paddockRed);
+
+        Paddock paddockYellow = new Paddock("Arnold", false);
+        paddockRepository.save(paddockYellow);
+
+        Paddock paddockPurple = new Paddock("Nedry", false);
+        paddockRepository.save(paddockPurple);
 
         Herbivore herbivore1 = new Herbivore("Archie", true,100, HerbivoreType.ATOPODENTATUS, paddockGreen);
         dinosaurRepository.save(herbivore1);
@@ -41,8 +50,14 @@ public class DataLoader implements ApplicationRunner {
         Herbivore herbivore3 = new Herbivore("Stelios", true,100, HerbivoreType.STEGOSAURUS, paddockGreen);
         dinosaurRepository.save(herbivore3);
 
+        Herbivore herbivore4 = new Herbivore("Cendy", true,80, HerbivoreType.CENTROSAURUS, paddockGreen);
+        dinosaurRepository.save(herbivore4);
+
         Carnivore carnivore1 = new Carnivore("Mossom", false,50, CarnivoreType.MOSOSAURUS, paddockRed);
         dinosaurRepository.save(carnivore1);
+
+        Carnivore carnivore2 = new Carnivore("Valerie", false,50, CarnivoreType.VELOCIRAPTOR, paddockYellow);
+        dinosaurRepository.save(carnivore2);
 
         }
     }
