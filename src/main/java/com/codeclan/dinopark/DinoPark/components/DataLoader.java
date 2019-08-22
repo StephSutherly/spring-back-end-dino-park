772c5954e49fa328963f1629d640c632e5a1addd
Steph Scully <stephanielarascullion@yahoo.com>
2019-08-22 08:02:25 +0100

    More data

--- src/main/java/com/codeclan/dinopark/DinoPark/components/DataLoader.java
+++ src/main/java/com/codeclan/dinopark/DinoPark/components/DataLoader.java
@@ -47,10 +47,10 @@ public class DataLoader implements ApplicationRunner {
         Herbivore herbivore2 = new Herbivore("Tricia", true,100, HerbivoreType.TRICERATOPS, paddockGreen);
         dinosaurRepository.save(herbivore2);
 
-        Herbivore herbivore3 = new Herbivore("Stelios", true,100, HerbivoreType.STEGOSAURUS, paddockGreen);
+        Herbivore herbivore3 = new Herbivore("Stelios", true,100, HerbivoreType.STEGOSAURUS, paddockOrange);
         dinosaurRepository.save(herbivore3);
 
-        Herbivore herbivore4 = new Herbivore("Cendy", true,80, HerbivoreType.CENTROSAURUS, paddockGreen);
+        Herbivore herbivore4 = new Herbivore("Cendy", true,80, HerbivoreType.CENTROSAURUS, paddockBlue);
         dinosaurRepository.save(herbivore4);
 
         Carnivore carnivore1 = new Carnivore("Mossom", false,50, CarnivoreType.MOSOSAURUS, paddockRed);
@@ -58,6 +58,25 @@ public class DataLoader implements ApplicationRunner {
 
         Carnivore carnivore2 = new Carnivore("Valerie", false,50, CarnivoreType.VELOCIRAPTOR, paddockYellow);
         dinosaurRepository.save(carnivore2);
+
+
+        Herbivore herbivore5 = new Herbivore("Norman", true,60, HerbivoreType.TRICERATOPS, paddockGreen);
+        dinosaurRepository.save(herbivore5);
+
+        Herbivore herbivore6 = new Herbivore("Diane", true,100, HerbivoreType.DIPLODOCUS, paddockBlue);
+        dinosaurRepository.save(herbivore6);
+
+        Herbivore herbivore7 = new Herbivore("Simeon", true,20, HerbivoreType.CENTROSAURUS, paddockGreen);
+        dinosaurRepository.save(herbivore7);
+
+        Carnivore carnivore3 = new Carnivore("Timothy", false,80, CarnivoreType.PTERODACTYL, paddockPurple);
+        dinosaurRepository.save(carnivore3);
+
+        Carnivore carnivore4 = new Carnivore("Petunia", false,50, CarnivoreType.TYRANNOSAURUS_REX, paddockRed);
+        dinosaurRepository.save(carnivore4);
+
+        Carnivore carnivore5 = new Carnivore("Vinny", false,50, CarnivoreType.VELOCIRAPTOR, paddockYellow);
+        dinosaurRepository.save(carnivore5);
 
         }
     }
